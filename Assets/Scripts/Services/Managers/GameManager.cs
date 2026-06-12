676e0f100a39d749c7145165536da38514ce4763
Luke Dawkin <luke.dawkin@stu.mmu.ac.uk>
2026-06-12 21:35:12 +0100

    feat: Added Service Locator
    Added Service Locator and basic managers/singletons.

---
+++ Assets/Scripts/Services/Managers/GameManager.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Assets.Scripts.Services.Managers
+{
+	public class GameManager
+	{
+
+	}
+}
---
+++ Assets/Scripts/Services/Singletons/Settings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Assets.Scripts.Services.Singletons
+{
+	public class Settings : SingletonBase
+	{
+		public Settings()
+		{
+			Load();
+		}
+
+		public void Load()
+		{
+
+		}
+
+		public void Save()
+		{
+
+		}
+	}
+}
